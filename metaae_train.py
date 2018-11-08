2ff2f2bb51912ecd55e73030b434b3886cd1cd2b
dragen1860 <dragen@live.cn>
2018-11-08 20:06:59 +0800

    update_num for test in metaae.py

--- metaae_train.py
+++ metaae_train.py
@@ -71,7 +71,8 @@ def main(args):
 
             # we can get the representation before first update, after k update
             # and test the representation on merged(test_spt, test_qry) set
-            h_spt0, h_spt1, h_qry0, h_qry1 = net.finetuning(spt_x, spt_y, qry_x, qry_y)
+            h_spt0, h_spt1, h_qry0, h_qry1 = net.finetuning(spt_x, spt_y, qry_x, qry_y,
+                                                            update_num=args.update_num+5)
 
             visualh.update(h_spt0, h_spt1, h_qry0, h_qry1, spt_y, qry_y, global_step)
 
