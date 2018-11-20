351ad28c49952752cda93d59f0afc04200d48096
dragen1860 <dragen@live.cn>
2018-11-20 16:17:23 +0800

    midify test.py to make it test on more episodes once

--- test.py
+++ test.py
@@ -57,78 +57,92 @@ def test(args, net, device, viz=None):
     viz.line([[0, 0]], [0], win='ars_on_qry01', opts=dict(title='ars_on_qry01',
                                                           legend=['h_qry0', 'h_qry1']))
 
-    for batchidx, (spt_x, spt_y, qry_x, qry_y) in enumerate(db_test):
-        spt_x, spt_y, qry_x, qry_y = spt_x.to(device), spt_y.to(device), qry_x.to(device), qry_y.to(device)
-        assert spt_x.size(0) == 1
-        spt_x, spt_y, qry_x, qry_y = spt_x.squeeze(0), spt_y.squeeze(0), qry_x.squeeze(0), qry_y.squeeze(0)
 
-        for ft_step in range(0, 150, 3):
+    for ft_step in range(0, 150, 3):
+
+        hs_spt0, hs_spt1, hs_qry0, hs_qry1, ys_spt, ys_qry = [], [], [], [], [], []
+        for batchidx, (spt_x, spt_y, qry_x, qry_y) in enumerate(db_test):
+            spt_x, spt_y, qry_x, qry_y = spt_x.to(device), spt_y.to(device), qry_x.to(device), qry_y.to(device)
+            assert spt_x.size(0) == 1
+            spt_x, spt_y, qry_x, qry_y = spt_x.squeeze(0), spt_y.squeeze(0), qry_x.squeeze(0), qry_y.squeeze(0)
+
+
 
             # we can get the representation before first update, after k update
             # and test the representation on merged(test_spt, test_qry) set
             h_spt0, h_spt1, h_qry0, h_qry1, _, new_net = net.finetuning(spt_x, spt_y, qry_x, qry_y,
                                                                             ft_step, None)
 
+            hs_spt0.append(h_spt0)
+            hs_spt1.append(h_spt1)
+            hs_qry0.append(h_qry0)
+            hs_qry1.append(h_qry1)
+            ys_spt.append(spt_y)
+            ys_qry.append(qry_y)
+
+            if batchidx > 10:
+                break
 
 
-            # visualh.update(h_spt0, h_spt1, h_qry0, h_qry1, spt_y, qry_y, batchidx)
+        h_spt0, h_spt1, h_qry0, h_qry1, y_spt, y_qry = torch.cat(hs_spt0, dim=0), torch.cat(hs_spt1, dim=0), \
+                                            torch.cat(hs_qry0, dim=0), torch.cat(hs_qry1, dim=0), \
+                                            torch.cat(ys_spt, dim=0), torch.cat(ys_qry, dim=0)
+        print('test total size:', h_spt1.shape, h_qry1.shape, y_qry.shape)
+
+        # we will use the acquired representation to cluster.
+        # h_spt: [sptsz, h_dim]
+        # h_qry: [qrysz, h_dim]
+        h_qry0_np = h_qry0.detach().cpu().numpy()
+        h_qry1_np = h_qry1.detach().cpu().numpy()
+        qry_y_np = qry_y.detach().cpu().numpy()
+        h_qry0_pred = cluster.KMeans(n_clusters=args.n_way, random_state=0).fit(h_qry0_np).labels_
+        h_qry1_pred = cluster.KMeans(n_clusters=args.n_way, random_state=0).fit(h_qry1_np).labels_
+        h_qry0_ami = metrics.adjusted_mutual_info_score(qry_y_np, h_qry0_pred)
+        h_qry0_ars = metrics.adjusted_rand_score(qry_y_np, h_qry0_pred)
+        h_qry1_ami = metrics.adjusted_mutual_info_score(qry_y_np, h_qry1_pred)
+        h_qry1_ars = metrics.adjusted_rand_score(qry_y_np, h_qry1_pred)
+        print(batchidx, 'ami:', h_qry0_ami, h_qry1_ami)
+        print(batchidx, 'ami:', h_qry0_ars, h_qry1_ars)
+        viz.line([[h_qry0_ami, h_qry1_ami]], [ft_step], win='ami_on_qry01', update='append')
+        viz.line([[h_qry0_ars, h_qry1_ars]], [ft_step], win='ars_on_qry01', update='append')
 
 
-            # we will use the acquired representation to cluster.
-            # h_spt: [sptsz, h_dim]
-            # h_qry: [qrysz, h_dim]
-            h_qry0_np = h_qry0.detach().cpu().numpy()
-            h_qry1_np = h_qry1.detach().cpu().numpy()
-            qry_y_np = qry_y.detach().cpu().numpy()
-            h_qry0_pred = cluster.KMeans(n_clusters=args.n_way, random_state=0).fit(h_qry0_np).labels_
-            h_qry1_pred = cluster.KMeans(n_clusters=args.n_way, random_state=0).fit(h_qry1_np).labels_
-            h_qry0_ami = metrics.adjusted_mutual_info_score(qry_y_np, h_qry0_pred)
-            h_qry0_ars = metrics.adjusted_rand_score(qry_y_np, h_qry0_pred)
-            h_qry1_ami = metrics.adjusted_mutual_info_score(qry_y_np, h_qry1_pred)
-            h_qry1_ars = metrics.adjusted_rand_score(qry_y_np, h_qry1_pred)
-            print(batchidx, 'ami:', h_qry0_ami, h_qry1_ami)
-            print(batchidx, 'ami:', h_qry0_ars, h_qry1_ars)
-            viz.line([[h_qry0_ami, h_qry1_ami]], [ft_step], win='ami_on_qry01', update='append')
-            viz.line([[h_qry0_ars, h_qry1_ars]], [ft_step], win='ars_on_qry01', update='append')
-
-
-            # compute contigency matrix
-            # viz.heatmap(
-            #     X=np.outer(np.arange(1, 6), np.arange(1, 11)),
-            #     opts=dict(
-            #         columnnames=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
-            #         rownames=['y1', 'y2', 'y3', 'y4', 'y5'],
-            #         colormap='Electric',
-            #         title='heatmap'
-            #     )
-            # )
-            h_qry0_cm = metrics.cluster.contingency_matrix(h_qry0_pred, qry_y)
-            h_qry1_cm = metrics.cluster.contingency_matrix(h_qry0_pred, qry_y)
-            # viz.heatmap(X=h_qry0_cm, win=args.exp+' h_qry0_cm', opts=dict(title=args.exp+' h_qry0_cm:%d'%batchidx,
-            #                                                               colormap='Electric'))
-            # viz.heatmap(X=h_qry1_cm, win=args.exp+' h_qry1_cm', opts=dict(title=args.exp+' h_qry1_cm:%d'%batchidx,
-            #                                                               colormap='Electric'))
-
-
-
-            acc0 = net.classify_train(h_spt0, spt_y, h_qry0, qry_y, use_h=True, train_step=args.classify_steps)
-            acc1 = net.classify_train(h_spt1, spt_y, h_qry1, qry_y, use_h=True, train_step=args.classify_steps)
-            print(batchidx, 'acc:\n', acc0, '\n', acc1)
-            viz.line([[acc0[-1], acc1[-1]]], [ft_step], win='acc_on_qry01', update='append')
+        # compute contigency matrix
+        # viz.heatmap(
+        #     X=np.outer(np.arange(1, 6), np.arange(1, 11)),
+        #     opts=dict(
+        #         columnnames=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+        #         rownames=['y1', 'y2', 'y3', 'y4', 'y5'],
+        #         colormap='Electric',
+        #         title='heatmap'
+        #     )
+        # )
+        # h_qry0_cm = metrics.cluster.contingency_matrix(h_qry0_pred, qry_y)
+        # h_qry1_cm = metrics.cluster.contingency_matrix(h_qry0_pred, qry_y)
+        # viz.heatmap(X=h_qry0_cm, win=args.exp+' h_qry0_cm', opts=dict(title=args.exp+' h_qry0_cm:%d'%batchidx,
+        #                                                               colormap='Electric'))
+        # viz.heatmap(X=h_qry1_cm, win=args.exp+' h_qry1_cm', opts=dict(title=args.exp+' h_qry1_cm:%d'%batchidx,
+        #                                                               colormap='Electric'))
 
 
 
-            spt_x_hat0 = net.forward_ae(spt_x[:64])
-            qry_x_hat0 = net.forward_ae(qry_x[:64])
-            spt_x_hat1 = new_net.forward_ae(spt_x[:64])
-            qry_x_hat1 = new_net.forward_ae(qry_x[:64])
-            viz.images(qry_x[:64], nrow=8, win=exp+'qry_x', opts=dict(title=exp+'qry_x:%d'%ft_step))
-            # viz.images(spt_x_hat0, nrow=8, win=exp+'spt_x_hat0', opts=dict(title=exp+'spt_x_hat0'))
-            viz.images(qry_x_hat0, nrow=8, win=exp+'qry_x_hat0', opts=dict(title=exp+'qry_x_hat0:%d'%ft_step))
-            # viz.images(spt_x_hat1, nrow=8, win=exp+'spt_x_hat1', opts=dict(title=exp+'spt_x_hat1'))
-            viz.images(qry_x_hat1, nrow=8, win=exp+'qry_x_hat1', opts=dict(title=exp+'qry_x_hat1:%d'%ft_step))
+        acc0 = net.classify_train(h_spt0, spt_y, h_qry0, qry_y, use_h=True, train_step=args.classify_steps)
+        acc1 = net.classify_train(h_spt1, spt_y, h_qry1, qry_y, use_h=True, train_step=args.classify_steps)
+        print(batchidx, 'acc:\n', acc0, '\n', acc1)
+        viz.line([[acc0[-1], acc1[-1]]], [ft_step], win='acc_on_qry01', update='append')
+
+
+
+        spt_x_hat0 = net.forward_ae(spt_x[:64])
+        qry_x_hat0 = net.forward_ae(qry_x[:64])
+        spt_x_hat1 = new_net.forward_ae(spt_x[:64])
+        qry_x_hat1 = new_net.forward_ae(qry_x[:64])
+        viz.images(qry_x[:64], nrow=8, win=exp+'qry_x', opts=dict(title=exp+'qry_x:%d'%ft_step))
+        # viz.images(spt_x_hat0, nrow=8, win=exp+'spt_x_hat0', opts=dict(title=exp+'spt_x_hat0'))
+        viz.images(qry_x_hat0, nrow=8, win=exp+'qry_x_hat0', opts=dict(title=exp+'qry_x_hat0:%d'%ft_step))
+        # viz.images(spt_x_hat1, nrow=8, win=exp+'spt_x_hat1', opts=dict(title=exp+'spt_x_hat1'))
+        viz.images(qry_x_hat1, nrow=8, win=exp+'qry_x_hat1', opts=dict(title=exp+'qry_x_hat1:%d'%ft_step))
 
 
         # only test for one episode.
         break
-
